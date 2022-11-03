2c46383d8310b1f558adf78fa6ffdb3990d4118a
Carla Gonçalves <goncalves.carla1998@gmail.com>
2022-11-03 16:14:36 +0000

    added extension in eventsHandler js file

--- frontend/src/pages/EventsPage/EventsPage.jsx
+++ frontend/src/pages/EventsPage/EventsPage.jsx
@@ -1,5 +1,6 @@
 import EventsList from "@components/EventsList/EventsList";
 import React from "react";
+// eslint-disable-next-line import/no-unresolved
 import "./eventsPage.css";
 
 function EventsPage() {
